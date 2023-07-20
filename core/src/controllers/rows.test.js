1c7037e0fada45fc0c69a2f22030a519f5f58e21
AbegaM <ebenezermelkamu@gmail.com>
2023-07-20 13:13:14 +0300

    Add function to change params to queryString

--- core/src/controllers/rows.test.js
+++ core/src/controllers/rows.test.js
@@ -3,6 +3,14 @@ const supertest = require('supertest');
 const app = require('../index');
 const requestWithSupertest = supertest(app);
 
+function queryString(params) {
+  const queryString = Object.keys(params)
+    .map((key) => `${key}=${params[key]}`)
+    .join('&');
+
+  return queryString;
+}
+
 describe('Rows Endpoints', () => {
   it('GET /tables/:name/rows should return a list of all rows', async () => {
     const res = await requestWithSupertest.get('/api/tables/users/rows');
@@ -17,17 +25,16 @@ describe('Rows Endpoints', () => {
   });
 
   it('GET /tables/:name/rows?_limit=8&_schema=firstName,lastName&_ordering:-firstName&_page=2: should query the rows by the provided query params', async () => {
-    const baseUrl = '/api/tables/users/rows';
     const params = {
-      _limit: 8,
-      _schema: 'firstName,lastName',
-      _ordering: '-firstName',
       _search: 'a',
+      _ordering: '-firstName',
+      _schema: 'firstName,lastName',
+      _limit: 8,
       _page: 2,
     };
-
+    const query = queryString(params);
     const res = await requestWithSupertest.get(
-      `${baseUrl}?${new URLSearchParams(params).toString()}`
+      `/api/tables/users/rows?${query}`
     );
 
     expect(res.status).toEqual(200);
@@ -38,14 +45,14 @@ describe('Rows Endpoints', () => {
     expect(res.body.data[0]).toHaveProperty('lastName');
 
     expect(res.body.next).toEqual(
-      `${baseUrl}?${new URLSearchParams({
+      `/tables/users/rows?${queryString({
         ...params,
         _page: params._page + 1,
       }).toString()}`
     );
 
     expect(res.body.previous).toEqual(
-      `${baseUrl}?${new URLSearchParams({
+      `/tables/users/rows?${queryString({
         ...params,
         _page: params._page - 1,
       }).toString()}`
