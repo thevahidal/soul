381fb2b48bcc907f6488d062eb66cfb2ea926fbe
Vahid Al <thevahidal@gmail.com>
2023-12-01 02:17:03 +0330

    Add Husky

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
--- src/index.js
+++ src/index.js
@@ -1,42 +1,40 @@
 #! /usr/bin/env node
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const winston = require('winston');
-const expressWinston = require('express-winston');
-const cors = require('cors');
-const rateLimit = require('express-rate-limit');
-const swaggerUi = require('swagger-ui-express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const winston = require("winston");
+const expressWinston = require("express-winston");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const swaggerUi = require("swagger-ui-express");
 
-const config = require('./config/index');
-const db = require('./db/index');
-const rootRoutes = require('./routes/index');
-const tablesRoutes = require('./routes/tables');
-const rowsRoutes = require('./routes/rows');
-const swaggerFile = require('./swagger/swagger.json');
-const { setupExtensions } = require('./extensions');
+const config = require("./config/index");
+const db = require("./db/index");
+const rootRoutes = require("./routes/index");
+const tablesRoutes = require("./routes/tables");
+const rowsRoutes = require("./routes/rows");
+const swaggerFile = require("./swagger/swagger.json");
+const { setupExtensions } = require("./extensions");
 
 const app = express();
 
-app.get('/health', (req, res) => {
-  res.send('OK');
+app.get("/health", (req, res) => {
+  res.send("OK");
 });
 
 app.use(bodyParser.json());
 
 // Activate wal mode
-db.pragma('journal_mode = WAL');
+db.exec("PRAGMA journal_mode = WAL");
 
 // Enable CORS
 let corsOrigin = config.cors.origin;
 
-if (corsOrigin.includes('*')) {
-  corsOrigin = '*';
+if (corsOrigin.includes("*")) {
+  corsOrigin = "*";
 }
 
-const corsOptions = {
-  origin: corsOrigin,
-};
+const corsOptions = { origin: corsOrigin };
 
 app.use(cors(corsOptions));
 
@@ -47,16 +45,17 @@ if (config.verbose !== null) {
       transports: [new winston.transports.Console()],
       format: winston.format.combine(
         winston.format.colorize(),
-        winston.format.json()
+
+        winston.format.json(),
       ),
       meta: false,
-      msg: 'HTTP {{req.method}} {{req.url}}',
+      msg: "HTTP {{req.method}} {{req.url}}",
       expressFormat: true,
+
       colorize: false,
-    })
+    }),
   );
 }
-
 if (config.rateLimit.enabled) {
   const limiter = rateLimit({
     windowMs: config.rateLimit.windowMs,
@@ -69,10 +68,10 @@ if (config.rateLimit.enabled) {
   app.use(limiter);
 }
 
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use('/api', rootRoutes);
-app.use('/api/tables', tablesRoutes);
-app.use('/api/tables', rowsRoutes);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/api", rootRoutes);
+app.use("/api/tables", tablesRoutes);
+app.use("/api/tables", rowsRoutes);
 
 setupExtensions(app, db);
 
